3566aee2e8d8b0795d5bed28bd7ca38f79860b1e
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-07-27 09:32:01 +0100

    Holiday club update

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ include("nav.html")
 ?>
 <h1>Welcome!</h1>
 <div style="padding-bottom: 30px">
-<img src="images/ctiflogo.jpg" align="left" height="100" style="padding-right: 20px">
+<img src="images/ctiflogo.jpg" align="left" height="100">
 <p>This is the website of Christians Together in Fareham - the churches in Fareham
 working together in partnership to proclaim the Good News about Jesus, and to
 bless our community through what we say and what we do.
@@ -19,10 +19,20 @@ bless our community through what we say and what we do.
 what we do, or about any of our member churches, please 
 <a href="mailto:enquiries@farehamchristians.org.uk">send us an email.</a>
 </div>
+<div>
+<h2>Holiday Club is under way!</h2>
+<img src="images/brightsparks.jpg" align="right" height="200px" >
+Our ecumenical summer holiday club, for children in school years 1-6, started on Monday
+and runs until Friday 28th July.
+We have 58 children attending, and lots of volunteers drawn from many different churches.
+<p>
+The theme is <b>Bright Sparks</b>, and focuses on Jesus, the Light of the World, and how
+he brings light into our lives. Our memory verse is taken from John 8:12 - 
+<i>I am the light of the world. Whoever follows me will not walk in darkness, but will
+have the light of life.</i>
+<p>Please pray for everyone involved.
+</div>
 <h2>Coming soon</h2>
-<p>Our next big event is the <b>Summer Holiday Club</b> for children aged 5-11.
-This takes place during the first week of the school summer holidays (24th-28th
-July), and bookings are now being taken. Please see the poster below for more information.
 <p>Our next meeting is on Tuesday 19th September, at 7.30pm, at Fareham United Reformed Church, 
 Osborn Road South PO16 7DG. Everyone is welcome.
 <h2>Latest news</h2>
@@ -30,6 +40,5 @@ Osborn Road South PO16 7DG. Everyone is welcome.
 <p><b>Christian Aid Week</b> raised almost £4000 in Fareham, through collections
 in churches, a street collection in Portchester and our stall in Fareham Shopping Centre.
 Many thanks to all who helped and all who gave.
-<img src="images/brightsparks.jpg" style="padding-top: 20px">
 </body>
 </html>
