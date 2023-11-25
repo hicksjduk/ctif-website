bb97a37f4c29c7d4eb59cf80aa7877ce2bb53a00
Jeremy Hicks <jeremy@thehickses.org.uk>
2023-11-25 11:11:11 +0000

    Update

--- public/index.php
+++ public/index.php
@@ -35,18 +35,10 @@ have the light of life.</i>
 please <a href="/holclub2023.php">visit this page.</a>
 </div>
 <h2>Coming soon</h2>
-<img src="images/christingle-orange-12544519.jpg" align="left" height="80px">
-<p>We will be holding a <b>Christingle stall</b> in West Street, as part of the festivities
-around the Christmas lights switch on on Saturday 18th November. This is a wonderful opportunity
-to engage with children and families about the true meaning of Christmas - the birth of Jesus,
-the Light of the World.
-<br>Volunteers to help with the stall will be most welcome - please email 
-<a href="mailto:chair@farehamchristians.org.uk">Rachel Hicks</a> if you would like to offer
-to help, or to know more.
-<p>Our next meeting is on <b>Monday 20th November</b>, at 7.15 for 7.30pm, at St Philip Howard Church, 
-Bishopsfield Road PO14 1QS. Everyone is welcome. <u>Please note</u> that this has been 
-rearranged from the date that was originally set (Wednesday 8th November), in order to avoid a 
-clash with the
-licensing of Rev Sam Cullen at Holy Trinity.
+<p><b>Christmas is coming!</b> All the churches in Fareham will soon be celebrating the
+season of Advent, which starts on 3rd December, in preparation to welcome the new-born
+King at Christmas. Details of services for Advent and Christmas can be found on the
+websites of the different churches, which are listed on our <a href="/members.php">
+Member churches</a> page.
 </body>
 </html>
