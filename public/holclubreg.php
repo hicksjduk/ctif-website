30a2ce6e4f03eeaeec2114e6540c9b1bbfccd5a3
Jeremy Hicks <jeremy@thehickses.org.uk>
2024-03-13 14:37:56 +0000

    Holiday club registration page

---
+++ public/holclubreg.php
@@ -0,0 +1,38 @@
+<!DOCTYPE php>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="site.css" rel="stylesheet" type="text/css">
+<title>Holiday Club 2024</title>
+</head>
+<body>
+<?php
+include("nav.html")
+?>
+<h1>Holiday Club 2024</h1>
+<h2>When is it?</h2>
+Monday 29 July to Friday 2nd August - 9.45am to 1.15pm. On Friday 2nd August, parents/carers are invited to come along at 12.45pm to watch the 'All Together Activity'. 
+<h2>Where is it?</h2>
+The Holiday Club is based at Gateway Church Fareham (171-173 West Street), but activities will also take place at Fareham United Reformed Church and Fareham Methodist Church.
+You will always drop off your child and pick them up from Gateway Church. Your child will be escorted safely to the other venues. 
+<h2>How much does it cost?</h2>
+The cost of the week is £15 per child. However, if the cost for your child/children is a challenge for you, please let us know as we may be able to help. 
+<h2>Is food provided?</h2>
+Yes. The children will be given a snack during the morning, as well as lunch. 
+<h2>How do I book a place for my child?</h2> 
+<ol>
+<li><a href="https://forms.office.com/r/9BqjgCgjbV">Complete an online registration form</a>, 
+or complete a paper copy and hand it in to the office at Holy Trinity Church, 132 West Street, 
+between 9am and 12 noon on weekdays. 
+<li>Pay the booking fee of £15, which can be paid:
+<ul>
+<li>by bank transfer to sort code 30-93-17, account number 00055620. Please use the reference <i>HC Surname</i>, where <i>Surname</i> is your child's surname. 
+(For example, for a booking for Tommy Smith, the reference will be <i>HC Smith</i>.) 
+<li>by cash to the office at Holy Trinity Church (address above).
+</ul>
+</ol>
+Once your form and payment have been received, we will be in touch to confirm your child's place at the Holiday Club.
+<h2>Any other questions?</h2>
+Contact Debs on 07749 023776, or email <a href="mailto:holidayclub@farehamchristians.org.uk">holidayclub@farehamchristians.org.uk</a>
+</body>
+</html>
