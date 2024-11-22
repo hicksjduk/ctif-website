35b2374f4f02bd3fccc7a595da9d5c02224d9aea
Jeremy Hicks <jeremy@thehickses.org.uk>
2024-11-22 16:27:09 +0000

    Update

--- public/index.php
+++ public/index.php
@@ -19,30 +19,20 @@ bless our community through what we say and what we do.
 what we do, or about any of our member churches, please 
 <a href="mailto:enquiries@farehamchristians.org.uk">send us an email.</a>
 </div>
-<div>
-<h2>Statement about recent events</h2>
-<p><i>Issued on 6th August 2024</i>
-<p><b>In the wake of the shocking attack on young people in Southport last week, 
-and the violence that has followed in several towns and cities, Christians Together in Fareham 
-want to express our love, prayers and solidarity with all the victims. As people of prayer, we 
-continue to pray for those who have been injured and bereaved; as people of the truth, we reject 
-all attempts to make dishonest political capital out of the suffering of the Southport victims; 
-as people of justice, we condemn the scapegoating of asylum seekers, Muslims, people of colour 
-and others, who bear no responsibility for what happened. We commit ourselves afresh to standing 
-against hatred and violence in every form, and doing all we can to build safe, caring and strong communities.
-</b>
-<h2>Latest news</h2>
-<table>
-<tr>
-<td><a href="holclubreg.php"><img src="images/HolClub24.jpg" align="right" height="150"></a>
-<p>The CTiF <b>Children's Holiday Club</b> was a huge success once again. Thank you to everyone
-who took part, and everyone who prayed for us. The children had a wonderful time, engaging
-with all the activities, singing and dancing to the songs, hearing the message of God's love
-for them, and learning the memory verse: "A new commandment I give you: love one another as
-I've loved you."
-</p>
-</td>
-</tr>
-</table>
+<h2>Upcoming events</h2>
+<div style="padding-bottom: 50px">
+<p><img src="images/Christingle.jpg" align="left" height="100" style="padding-right: 20px">
+We are running a <b>Christingle stall</b> as part of the festivities for
+the Christmas lights switch-on in Fareham, on Saturday 23rd November from 11am.
+We will be indoors, so don't let Storm Bert put you off - come and see us!  
+</div>
+<div style="padding-bottom: 30px">
+<p>
+<h3>Christmas is coming!</h3> 
+<p>Visit <a href="members.php">our member churches' websites</a> to find out when their Christmas services are.
+<p><a href="images/Carols Poster.jpg"><img src="images/Carols Poster.jpg" align="left" height="100" style="padding-right: 20px"></a>
+We will be <b>singing carols in Fareham shopping precinct</b> on Saturday 21st December, 10.30 to 11.30am.
+Come and join us! Click the poster for more details.
+</div>
 </body>
 </html>
