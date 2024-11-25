dc261ae54e6405d00c84a448d4929dc778b598cd
Jeremy Hicks <jeremy@thehickses.org.uk>
2024-11-25 11:34:39 +0000

    Testing

---
+++ public/test.php
@@ -0,0 +1,16 @@
+<!DOCTYPE php>
+<html>
+<head>
+<meta charset="UTF-8">
+<link href="site.css" rel="stylesheet" type="text/css">
+<title>Test</title>
+</head>
+<body>
+<?php
+include("nav.html")
+?>
+<?php
+echo date_parse("2019-12-09")
+?>
+</body>
+</html>
