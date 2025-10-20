d2a80801dccf03ab4be87df358e27e6e3d92b8fc
Jeremy Hicks <jeremy@thehickses.org.uk>
2025-10-20 14:26:07 +0100

    Update who's who

--- public/whoswho.php
+++ public/whoswho.php
@@ -13,39 +13,36 @@ include("nav.html")
 <p>Click on the person's name to send them an email.
 <div id="whoswho">
 <div>
-<img src="images/Rachel.jpg" align="left" height="150" style="padding-right: 20px">
+<img src="images/Rachel.jpg" align="left" height="200" style="padding-right: 20px">
 <h2><a href="mailto:chair@farehamchristians.org.uk">Rachel Hicks</a> - Chair</h2>
-<p>I worship at St John's Church, where I am the leader of the pastoral support team. 
+<p>I worship at St John's Church. 
 I am married to Jeremy, and we have a son and a grandson.
 I trained as an occupational therapist, and have worked as a mental health OT in 
 hospital and community settings; more recently I was a special needs assistant in 
 primary schools, working with visually-impaired children. I currently lead the team that 
-organises Christians Together's annual summer holiday club for children. I love being 
+organises Christians Together's annual summer holiday club for children. 
+<p>I love being 
 part of Christians Together in Fareham, because I am passionate about Christians working 
 together to spread the Good News of Jesus in both word and deed.
 </div>
 <div>
-<img src="images/John.jpg" align="left" height="150" style="padding-right: 20px">
-<h2><a href="mailto:treasurer@farehamchristians.org.uk">John Dear</a> - Treasurer</h2>
-<p>John has been a Christian since 1978 and a member of Fareham Baptist New Life Church since 2009. 
-He is part of the worship group playing keyboard and guitar and oversees the Audio-Visual team for live-streaming
-services. Previously he has been a member of Locks Heath Free Church, Thornhill Baptist Church 
-and Southampton Community Church.
-<br>For work John is a self-employed IT Consultant and Fellow of the Institution of Analysts and Programmers,
-although he started his working life as a car mechanic. He also holds qualifications from the Association of 
-Accounting Technicians.
-<br>John is married with two grown-up sons. In his spare time John enjoys watching 
-Snooker, Rugby and (before Abu Dhabi) F1.
+<!--img src="images/Liz.jpg" align="left" height="150" style="padding-right: 20px"-->
+<h2><a href="mailto:treasurer@farehamchristians.org.uk">Liz Dunning</a> - Treasurer</h2>
+<p>I am a member of Fareham United Reformed Church where I am secretary 
+and assistant treasurer. I love that CTiF brings us all together with  
+Lent groups, ecumenical services and Christingle making! 
+<p>My favourite pastime is camping but I also love gardening and cooking.
 </div>
 <div>
 <img src="images/Jeremy.jpg" align="left" height="150" style="padding-right: 20px">
 <h2><a href="mailto:secretary@farehamchristians.org.uk">Jeremy Hicks</a> - Secretary</h2>
 <p>I worship at St John's Church where I am a Reader (lay preacher) and music leader. 
 Before retirement I was a software engineer in the digital television industry. I enjoy cycling, 
-playing and watching sport, travel, beer, and the annual Greenbelt festival.
+playing and watching sport, travel, beer, and 
+<a href="https://www.greenbelt.org.uk/">the annual Greenbelt festival</a>.
 I met Rachel at an ecumenical Lent study group in Fareham, and we have been married 
-for over thirty years. <!--; we have a son and a grandson.--> 
-I'm excited to be part of CTiF because of the many different ways in which we work together to bless our community.
+for over thirty years. 
+<p>I'm excited to be part of CTiF because of the many different ways in which we work together to bless our community.
 </div>
 </div>
 </body>
